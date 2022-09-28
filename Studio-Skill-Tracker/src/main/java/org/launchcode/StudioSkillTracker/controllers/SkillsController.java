425df39719ff98a30cce4c10f704b6828fbf8d87
Thara Rajesh <thararaje@gmail.com>
2022-09-27 21:35:26 -0500

    skill controller studio done

--- Studio-Skill-Tracker/src/main/java/org/launchcode/StudioSkillTracker/controllers/SkillsController.java
+++ Studio-Skill-Tracker/src/main/java/org/launchcode/StudioSkillTracker/controllers/SkillsController.java
@@ -1,8 +1,9 @@
 package org.launchcode.StudioSkillTracker.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
 
 @Controller
 public class SkillsController {
@@ -10,7 +11,7 @@ public class SkillsController {
     // three programming languages of your choosing.
     @GetMapping
     @ResponseBody
-    public String createMainPage(){
+    public String createMainPage() {
         return "<html>" + "<body>" +
                 "<h1> Skills Tracker </h1>" +
                 "<h2> We have a few skills we would like to learn. Here is the list!</h2>" +
@@ -21,39 +22,55 @@ public class SkillsController {
                 "</ol>" +
                 "</body>" + "</html>";
     }
+
     //At localhost:8080/form, add a form that lets the user enter their name and choose their favorite,
     // second favorite, and third favorite programming languages on your list.
     // Use select elements for each of the rankings. Just as with the exercises,
     // we will use @GetMapping()
     @GetMapping("/form")
     @ResponseBody
-    public String createForm(){
+    public String createForm() {
         return "<html>" +
                 "<body>" +
                 "<form action = '/form' method = 'post'>" +
                 "<label><b> Name: <br><input type='text' name= 'name'></label><br>" +
                 "<label><b> My favorite language: </label><br>" +
-                "<select name='My first-language' id='first-language'>"+
-                "<option value='java'>Java</option>"+
-                "<option value='javascript'>Javascript</option>"+
-                "<option value='python'>Python</option>"+
-                "</select>"+
+                "<select name='firstlanguage' id='firstlanguage'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Javascript'>Javascript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select>" +
                 "<label><b><br> My Second favorite language: </label><br>" +
-                "<select name='second-language' id='second-language'>"+
-                "<option value='java'>Java</option>"+
-                "<option value='javascript'>Javascript</option>"+
-                "<option value='python'>Python</option>"+
+                "<select name='secondlanguage' id='secondlanguage'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Javascript'>Javascript</option>" +
+                "<option value='Python'>Python</option>" +
                 "</select>" +
                 "<label><b><br> My Third favorite language: </label><br>" +
-                "<select name='third-language' id='third-language'>"+
-                "<option value='java'>Java</option>"+
-                "<option value='javascript'>Javascript</option>"+
-                "<option value='python'>Python</option>"+
+                "<select name='thirdlanguage' id='thirdlanguage'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='Javascript'>Javascript</option>" +
+                "<option value='Python'>Python</option>" +
                 "</select>" +
                 "<br><input type='submit', value='Submit'><br>" +
                 "</form>" + "</body>" + "</html>";
 
 
     }
-    }
 
+    @RequestMapping(value="/form", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String firstlanguage
+            , @RequestParam String secondlanguage
+            , @RequestParam String thirdlanguage) {
+        String html = "<html>" + "<body>" +
+                "<h1> {0} </h1>" +
+                "<b><ol>" +
+                "<li> {1} </li>" +
+                "<li> {2} </li>" +
+                "<li> {3} </li>" +
+                "</ol></b>" +
+                "</body>" + "</html>";
+        return MessageFormat.format(html, name,firstlanguage, secondlanguage, thirdlanguage);
+    }
+}
